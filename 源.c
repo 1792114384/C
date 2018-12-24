d6b0ff8c45cc54a7f62b88ff004fee12c3c9ca9f
The Laughing Uncle <33725245+1792114384@users.noreply.github.com>
2018-12-24 17:27:43 +0800

    Add files via upload

--- 源.c
+++ 源.c
@@ -1,15 +1,10 @@
 #pragma warning(disable:4996)
 #include <stdio.h>
 #include <stdlib.h>
-#include <windows.h>
+
 int main()
 {
-	for (int i = 0; i < 20; i++)
-	{
-		Sleep(100);
-		printf("#");
-	}
-	system("cls");
+	printf("%d", 1 & 2);
 	system("pause");
 	return 0;
 }
