128832719667cc7f0e8a7225b99327b7c5e305d7
The Laughing Uncle <33725245+1792114384@users.noreply.github.com>
2018-12-25 20:52:47 +0800

    Add files via upload

--- text.c
+++ text.c
@@ -2,16 +2,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void Reverse(int arr[],int sz)
+{
+	int left = 0;
+	int right = sz-1;
+	while (left < right)
+	{
+		int tmp = arr[left];
+		arr[left] = arr[right];
+		arr[right] = tmp;
+		left++;
+		right--;
+	}
+}
+
+void Print(int arr[], int sz)
+{
+	int *p = arr;
+	for (int i = 0; i < sz; i++)
+	{
+		printf("%d ", *(p + i));
+	}
+}
+
 int main()
 {
-	int ch = 0;
-	while ((ch = getchar()) != EOF)
-	{
-		if (ch >= 'a'&&ch <= 'z')
-			printf("%c", ch - ('a' - 'A'));
-		if (ch >= 'A'&&ch <= 'Z')
-			printf("%c", ch + ('a' - 'A'));
-	}
+	int arr[10] = { 1,2,3,4,5,6,7,8,9,10};
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	Reverse(arr, sz);
+	Print(arr, sz);
 	system("pause");
 	return 0;
 }
