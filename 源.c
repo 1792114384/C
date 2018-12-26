e1b8751511020eb487359a8efe1a6cfd79dbfe20
The Laughing Uncle <33725245+1792114384@users.noreply.github.com>
2018-12-26 12:53:31 +0800

    Add files via upload

--- 源.c
+++ 源.c
@@ -1,47 +1,13 @@
 #pragma warning(disable:4996)
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-void InitArr(int arr[], int sz)
-{
-	for (int i = 0; i < sz; i++)
-		arr[i] = i;
-}
-
-void PrintArr(int arr[], int sz)
-{
-	int *p = arr;
-	for (int i = 0; i < sz; i++)
-		printf("%d ", *(p+i));
-}
-
-
-void EmptyArr(int arr[], int sz)
-{
-	memset(arr, -1, 40);
-}
-
-void ReverseArr(int arr[], int sz)
-{
-	for (int i = 0; i < sz / 2; i++)
-	{
-		int tmp = arr[i];
-		arr[i] = arr[sz - 1 - i];
-		arr[sz - 1 - i] = tmp;
-	}
-}
 
 int main()
 {
 	int arr[10] = { 0 };
-	int sz = sizeof(arr) / sizeof(arr[0]);
-	InitArr(arr, sz);
-	PrintArr(arr, sz);
-	printf("\n");
-	ReverseArr(arr, sz);
-	PrintArr(arr, sz);
-	printf("\n");
+	for (int i = 0; i < sizeof(arr) / sizeof(arr[0]); i++)
+	{
+		printf("&arr[%d] = %p\n", i, &arr[i]);
+	}
 	system("pause");
 	return 0;
 }
