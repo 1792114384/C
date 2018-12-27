d1f7582d18026f9ccfb720248425818fae31c628
The Laughing Uncle <33725245+1792114384@users.noreply.github.com>
2018-12-27 18:11:18 +0800

    Add files via upload

--- test.c
+++ test.c
@@ -1,179 +1,95 @@
-//strlen
+#define _CRT_SECURE_NO_WARNINGS
+
 #include <stdio.h>
-#include <assert.h>
 #include <stdlib.h>
+//int main()
+//{
+//	int a[5] = { 1,2,3,4,5 };
+//	int *ptr = (int *)(&a + 1);
+//	printf("%d,%d", *(a + 1), *(ptr - 1));
+//	system("pause");
+//	return 0;
+//}
 
-int my_strlen(const char *str)
-{
-	int count = 0;
-	assert(str);
-	while (*str++)
-	{
-		count++;
-	}
-	return count;
-}
-
-int my_strlen(const char *str)
-{
-	assert(str);
-	if (*str == '\0')
-		return 0;
-	else
-		return 1 + my_strlen(++str);
-}
-
-int my_strlen(char *str)
-{
-	char *ret = str;
-	assert(str);
-	while (*ret != '\0')
-	{
-		ret++;
-	}
-	return ret - str;
-}
-
-int main()
-{
-	char arr[100] = "abcdef";
-	int ret = my_strlen(arr);
-	printf("%d\n", ret);
-	system("pause");
-	return 0;
-}
-//strcpy
+//struct Test
+//{
+//	int num;       //4字节
+//	char *pcName;  //4字节
+//	short sDate;   //2字节
+//	char cha[2];   //2字节
+//	short sBa[4];  //8字节
+//}*p;
 //
-char* my_strcpy(char *dest, const char *str)
-{
-	char *ret = dest;
-	assert(dest && str);
-	while (*dest++ = *str++)
-	{
-		;
-	}
-	return ret;
-}
+//int main()
+//{
+//	p = 0x100000;
+//	printf("%x\n", p + 0x1);//
+//	printf("%x\n", (unsigned long)p + 0x1);
+//	printf("%x\n", (unsigned int*)p + 0x1);
+//	system("pause");
+//	return 0;
+//}
+
+//int main()
+//{
+//	int a[4] = { 1,2,3,4 };
+//	int *ptr1 = (int *)(&a + 1);
+//	int *ptr2 = (int *)((int)a + 1);
+//	printf("%x %x", ptr1[-1], *ptr2);
+//	system("pause");
+//	return 0;
+//}
+
+//int main()
+//{
+//	int a[3][2] = { (0,1),(2,3),(4,5) };
+//	int *p;
+//	p = a[0];
+//	printf("%d", p[0]);
+//	system("pause");
+//	return 0;
+//}
+
+//int main()
+//{
+//	int a[5][5];
+//	int(*p)[4];
+//	p = a;
+//	printf("%p,%d\n", &p[4][2] - &a[4][2], &p[4][2] - &a[4][2]);
+//	system("pause");
+//	return 0;
+//}
+
+//int main()
+//{
+//	int aa[2][5] = { 1,2,3,4,5,6,7,8,9,10 };
+//	int *ptr1 = (int *)(&aa + 1);
+//	int *ptr2 = (int *)(*(aa + 1));
+//	printf("%d,%d", *(ptr1 - 1), *(ptr2 - 1));
+//	system("pause");
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	char *a[] = { "work","at","alibaba" };
+//	char**pa = a;
+//	pa++;
+//	printf("%s\n", *pa);
+//	system("pause");
+//	return 0;
+//}
 
 int main()
 {
-	char arr1[100] = { 0 };
-	char arr2[] = "abcdef";
-	char *ret = my_strcpy(arr1, arr2);
-	printf("%s\n", ret);
-	system("pause");
-	return 0;
-}
-//strcat
-
-char* my_strcpy(char *dest, char *str)
-{
-	char *ret = dest;
-	assert(dest && str);
-	while (*dest)
-	{
-		dest++;
-	}
-	while (*dest++ = *str++)
-	{
-		;
-	}
-	return ret;
-}
-
-int main()
-{
-	char arr1[100] = "abcdef";
-	char arr2[100] = "ghijk";
-	char *ret = my_strcpy(arr1, arr2);
-	printf("%s\n", ret);
-	system("pause");
-	return 0;
-}
-//strstr
-
-char* my_strstr(char *str1, char *str2)
-{
-	assert(str1 && str2);
-	char *start = str1;
-	char *cur = str2;
-	char *s1 = NULL;
-	if (*str2 == '\0')
-	{
-		return NULL;
-	}
-	else
-	{
-		while (*start)
-		{
-			s1 = start;
-			cur = str2;
-			while (*s1 && *cur && (*s1 == *cur))
-			{
-				s1++;
-				cur++;
-			}
-			if (*cur == '\0')
-			{
-				return start;
-			}
-			start++;
-		}
-	}
-}
-
-int main()
-{
-	char arr1[] = "abbcdefg";
-	char arr2[] = "bcd";
-	char *ret = my_strstr(arr1, arr2);
-	if (*ret)
-	{
-		printf("%s\n", ret);
-	}
-	else
-	{
-		printf("找不到\n");
-	}
-	system("pause");
-	return 0;
-}
-//strcmp
-
-int my_atrcmp(const char str1[], const char str2[])
-{
-	assert(str1 && str2);
-	int ret = 0;
-	while ( !(ret = *(unsigned char *)str1 - *(unsigned char *)str2) && *str1 )
-	{
-		str1++;
-		str2++;
-	}
-	if (ret < 0)
-		return -1;
-	else if (ret > 0)
-		return 1;
-	else
-		return 0;
-}
-
-int main()
-{
-	char arr1[] = "abcdef";
-	char arr2[] = "abcdef";
-	int ret = my_atrcmp(arr1, arr2);
-	if (0 == ret)
-	{
-		printf("相等\n");
-	}
-	else if (-1 == ret)
-	{
-		printf("第一个小\n");
-	}
-	else
-	{
-		printf("第一个大\n");
-	}
+	char *c[] = { "ENIER","NEW","POINT","FIRST" };
+	char**cp[] = { c + 3,c + 2,c + 1,c };
+	char***cpp = cp;
+	printf("%s\n", **++cpp);
+	printf("%s\n", *--*++cpp + 3);
+	printf("%s\n", *cpp[-2] + 3);
+	printf("%s\n", cpp[-1][-1] + 1);
 	system("pause");
 	return 0;
 }
