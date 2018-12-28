f2846650cfc89b42702b6b8cd9801a0d94bc712f
The Laughing Uncle <33725245+1792114384@users.noreply.github.com>
2018-12-28 15:22:15 +0800

    Add files via upload

--- test.c
+++ test.c
@@ -1,95 +1,211 @@
-#define _CRT_SECURE_NO_WARNINGS
+#define _CRT_SECURE_NO_WARNINGS 1
 
 #include <stdio.h>
 #include <stdlib.h>
-//int main()
-//{
-//	int a[5] = { 1,2,3,4,5 };
-//	int *ptr = (int *)(&a + 1);
-//	printf("%d,%d", *(a + 1), *(ptr - 1));
-//	system("pause");
-//	return 0;
-//}
 
-//struct Test
-//{
-//	int num;       //4字节
-//	char *pcName;  //4字节
-//	short sDate;   //2字节
-//	char cha[2];   //2字节
-//	short sBa[4];  //8字节
-//}*p;
+//#define X 3
+//#define Y X*2
+//#undef X
+//
+//#define X 2
 //
 //int main()
 //{
-//	p = 0x100000;
-//	printf("%x\n", p + 0x1);//
-//	printf("%x\n", (unsigned long)p + 0x1);
-//	printf("%x\n", (unsigned int*)p + 0x1);
+//	int z = Y;
+//	printf("%d\n", z);
 //	system("pause");
 //	return 0;
 //}
 
 //int main()
 //{
-//	int a[4] = { 1,2,3,4 };
-//	int *ptr1 = (int *)(&a + 1);
-//	int *ptr2 = (int *)((int)a + 1);
-//	printf("%x %x", ptr1[-1], *ptr2);
+//	printf("%d\n", 5 / (-3));
+//	printf("%d\n", (5) % (-6));
+//	system("pause");
+//	return 0;
+//}
+
+//
+//int main()
+//{
+//	int i = 0;
+//	int count = 0;
+//	for (i = 0,printf("First = %d\n",i);
+//	     i < 10,printf("Second = %d\n",i);
+//	     i++,printf("Third = %d\n",i))
+//	{
+//		printf("Fourth = %d\n", i);
+//		count++;
+//		if (count > 100)
+//		{
+//			break;
+//		}
+//	}
+//	system("pause");
+//	return 0;
+//}
+
+//union Un
+//{
+//	char c[5];
+//	int i;
+//};
+
+//union Un
+//{
+//	char c;
+//	int i;
+//};
+
+//enum Sex
+//{
+//	MALE,
+//	FAMALE,
+//	SECRET,
+//};
+
+//#pragma pack(2)//设置默认对齐数为8
+//struct S1
+//{
+//	char c1;
+//	int i;
+//	char c2;
+//};
+//#pragma pack()//取消设置的默认对齐数，还原为默认
+
+//struct S2
+//{
+//	double d;
+//	char c;
+//	int i;
+//};
+//
+//struct S3
+//{
+//	char c1;
+//	struct S2 s3;
+//	double d;
+//};
+//
+//int main()
+//{
+//	printf("%d\n", sizeof(struct S2));
+//	printf("%d\n", sizeof(struct S3));
+//	system("pause");
+//	return 0;
+//}
+
+//struct s1
+//{
+//	char c1;
+//	int i;
+//	char c2;
+//};
+
+//int CheckSystem()
+//{
+//	union check
+//	{
+//		int i;
+//		char ch;
+//	}c;
+//	c.i = 1;
+//	return (c.ch == 1);
+//}
+//
+//int main()
+//{
+//	int ret = 0;
+//	ret = CheckSystem();
+//	if (ret == 1)
+//	{
+//		printf("小端\n");
+//	}
+//	else
+//	{
+//		printf("大端\n");
+//	}
 //	system("pause");
 //	return 0;
 //}
 
 //int main()
 //{
-//	int a[3][2] = { (0,1),(2,3),(4,5) };
-//	int *p;
-//	p = a[0];
-//	printf("%d", p[0]);
+//	/*int i = -20;
+//	unsigned j = 10;
+//	printf("%d\n", i + j);*/
+//	unsigned i; 
+//	int count = 0;
+//	for (i = 9; i >= 0; i--) 
+//	{ 
+//		printf("%x\n", i);
+//		count++;
+//		if (count > 20)
+//			break;
+//	}
 //	system("pause");
 //	return 0;
 //}
 
+//void fun(int b[100])
+//{
+//	printf("%d\n", sizeof(b)); 
+//}
+//
 //int main()
 //{
-//	int a[5][5];
-//	int(*p)[4];
-//	p = a;
-//	printf("%p,%d\n", &p[4][2] - &a[4][2], &p[4][2] - &a[4][2]);
+//	int b[100];
+//	fun(b);
+//	int*p = NULL;
+//	printf("%d\n", sizeof(p));       //4
+//	printf("%d\n", sizeof(*p));       //4
+//
+//	int a[100];
+//	printf("%d\n", sizeof(a));       //
+//	printf("%d\n", sizeof(a[100]));  //4
+//	printf("%d\n", sizeof(&a));      //4
+//	printf("%d\n", sizeof(&a[0]));   //4
 //	system("pause");
 //	return 0;
+//
 //}
 
+
+
+//static int j;
+//
+//void fun1(void)
+//{
+//	static int i = 0;
+//	i++;
+//	printf("%d\n", i);
+//}
+//void fun2(void)
+//{
+//	j = 0;
+//	j++;
+//}
 //int main()
 //{
-//	int aa[2][5] = { 1,2,3,4,5,6,7,8,9,10 };
-//	int *ptr1 = (int *)(&aa + 1);
-//	int *ptr2 = (int *)(*(aa + 1));
-//	printf("%d,%d", *(ptr1 - 1), *(ptr2 - 1));
+//	for (int k = 0; k<10; k++)
+//	{
+//		fun1();
+//		fun2();
+//	}
+//	printf("%d\n", j);
 //	system("pause");
 //	return 0;
 //}
-
-
 //int main()
 //{
-//	char *a[] = { "work","at","alibaba" };
-//	char**pa = a;
-//	pa++;
-//	printf("%s\n", *pa);
+//	int n = 0;
+//	char *pc = (char *)&n;
+//	int *pi = &n;
+//	printf("%p\n", &n);
+//	printf("%p\n", pc);
+//	printf("%p\n", pc + 1);
+//	printf("%p\n", pi);
+//	printf("%p\n", pi + 1);
 //	system("pause");
 //	return 0;
 //}
-
-int main()
-{
-	char *c[] = { "ENIER","NEW","POINT","FIRST" };
-	char**cp[] = { c + 3,c + 2,c + 1,c };
-	char***cpp = cp;
-	printf("%s\n", **++cpp);
-	printf("%s\n", *--*++cpp + 3);
-	printf("%s\n", *cpp[-2] + 3);
-	printf("%s\n", cpp[-1][-1] + 1);
-	system("pause");
-	return 0;
-}
