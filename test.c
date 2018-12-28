2bdd43318529c9c2662236e1c6ec8f263ab92fae
The Laughing Uncle <33725245+1792114384@users.noreply.github.com>
2018-12-28 15:22:31 +0800

    Add files via upload

--- test.c
+++ test.c
@@ -1,211 +1,145 @@
-#define _CRT_SECURE_NO_WARNINGS 1
+#define _CRT_SECURE_NO_WARNINGS 
 
 #include <stdio.h>
 #include <stdlib.h>
 
-//#define X 3
-//#define Y X*2
-//#undef X
-//
-//#define X 2
-//
-//int main()
-//{
-//	int z = Y;
-//	printf("%d\n", z);
-//	system("pause");
-//	return 0;
-//}
-
-//int main()
-//{
-//	printf("%d\n", 5 / (-3));
-//	printf("%d\n", (5) % (-6));
-//	system("pause");
-//	return 0;
-//}
-
-//
-//int main()
-//{
-//	int i = 0;
-//	int count = 0;
-//	for (i = 0,printf("First = %d\n",i);
-//	     i < 10,printf("Second = %d\n",i);
-//	     i++,printf("Third = %d\n",i))
-//	{
-//		printf("Fourth = %d\n", i);
-//		count++;
-//		if (count > 100)
-//		{
-//			break;
-//		}
-//	}
-//	system("pause");
-//	return 0;
-//}
-
-//union Un
-//{
-//	char c[5];
-//	int i;
-//};
-
-//union Un
-//{
-//	char c;
-//	int i;
-//};
-
-//enum Sex
-//{
-//	MALE,
-//	FAMALE,
-//	SECRET,
-//};
-
-//#pragma pack(2)//设置默认对齐数为8
-//struct S1
-//{
-//	char c1;
-//	int i;
-//	char c2;
-//};
-//#pragma pack()//取消设置的默认对齐数，还原为默认
-
-//struct S2
-//{
-//	double d;
-//	char c;
-//	int i;
-//};
-//
-//struct S3
-//{
-//	char c1;
-//	struct S2 s3;
-//	double d;
-//};
-//
-//int main()
-//{
-//	printf("%d\n", sizeof(struct S2));
-//	printf("%d\n", sizeof(struct S3));
-//	system("pause");
-//	return 0;
-//}
-
-//struct s1
-//{
-//	char c1;
-//	int i;
-//	char c2;
-//};
-
-//int CheckSystem()
-//{
-//	union check
-//	{
-//		int i;
-//		char ch;
-//	}c;
-//	c.i = 1;
-//	return (c.ch == 1);
-//}
-//
-//int main()
+//int find_one(int *str, int sz)
 //{
 //	int ret = 0;
-//	ret = CheckSystem();
-//	if (ret == 1)
+//	for (int i = 0; i < sz; i++)
 //	{
-//		printf("小端\n");
+//		ret ^= *(str + i);
 //	}
-//	else
+//	return ret;
+//}
+
+void find_two(int *str, int sz, int *px, int *py)
+{
+	int ret = 0;
+	int pos = 0;
+	for (int i = 0; i < sz; i++)
+	{
+		ret ^= *(str + i);
+	}
+	for (int i = 0; i < 32; i++)
+	{
+		if (((ret >> i) & 1) == 1)
+		{
+			pos = i;
+			break;
+		}
+	}
+	for (int i = 0; i < sz; i++)
+	{
+		if (((*(str + i) >> pos) & 1) == 1)
+		{
+			*px ^= *(str + i);
+		}
+		else
+		{
+			*py ^= *(str + i);
+		}
+	}
+}
+
+int lastBitOf1(int num)
+{
+	return num & ~(num - 1);
+}
+
+void find_three(int *str, int sz, int *px, int *py, int *pz)
+{
+	int ret = 0;
+	int pos = 0;
+	int arr1[12];
+	int flag = 0;
+	for (int i = 0; i < sz; i++)
+	{
+		ret ^= *(str + i);
+		arr1[i] = *(str + i);
+	}
+	for (int i = 0; i < sz; i++) 
+	{
+		flag ^= lastBitOf1(ret ^ *(str + i));
+	}
+	for (int i = 0; i < sz; i++) {
+		if (lastBitOf1(*(str + i) ^ ret) == flag)
+			*px ^= *(str + i);
+	}
+	arr1[sz] = *px;
+	ret ^= *px;
+	for (int i = 0; i < 32; i++)
+	{
+		if (((ret >> i) & 1) == 1)
+		{
+			pos = i;
+			break;
+		}
+	}
+	for (int i = 0; i < sz + 1; i++)
+	{
+		if (((arr1[i] >> pos) & 1) == 1)
+		{
+			*py ^= arr1[i];
+		}
+		else
+		{
+			*pz ^= arr1[i];
+		}
+	}
+}
+
+int main()
+{
+	int arr[] = { 1,2,5,3,101,10,5,3,2,1,9 };
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	int num1 = 0;
+	int num2 = 0;
+	int num3 = 0;
+	find_three(arr, sz, &num1, &num2, &num3);
+	printf("%d %d %d\n", num1, num2, num3);
+	system("pause");
+	return 0;
+}
+
+//int int_cmp(const void*p1, const void*p2)
+//{
+//	return (*(int *)p1 > *(int *)p2);
+//}
+//
+//void swap(char *p1, char *p2, int size)
+//{
+//	for (int i = 0; i < size; i++)
 //	{
-//		printf("大端\n");
+//		char tmp = *(p1 + i);
+//		*(p1 + i) = *(p2+ i);
+//		*(p2+ i) = tmp;
 //	}
-//	system("pause");
-//	return 0;
-//}
-
-//int main()
-//{
-//	/*int i = -20;
-//	unsigned j = 10;
-//	printf("%d\n", i + j);*/
-//	unsigned i; 
-//	int count = 0;
-//	for (i = 9; i >= 0; i--) 
-//	{ 
-//		printf("%x\n", i);
-//		count++;
-//		if (count > 20)
-//			break;
-//	}
-//	system("pause");
-//	return 0;
-//}
-
-//void fun(int b[100])
-//{
-//	printf("%d\n", sizeof(b)); 
 //}
 //
-//int main()
+//void bubble_sort(void *base, int count, int size, int(*cmp)(void *, void *))
 //{
-//	int b[100];
-//	fun(b);
-//	int*p = NULL;
-//	printf("%d\n", sizeof(p));       //4
-//	printf("%d\n", sizeof(*p));       //4
-//
-//	int a[100];
-//	printf("%d\n", sizeof(a));       //
-//	printf("%d\n", sizeof(a[100]));  //4
-//	printf("%d\n", sizeof(&a));      //4
-//	printf("%d\n", sizeof(&a[0]));   //4
-//	system("pause");
-//	return 0;
-//
-//}
-
-
-
-//static int j;
-//
-//void fun1(void)
-//{
-//	static int i = 0;
-//	i++;
-//	printf("%d\n", i);
-//}
-//void fun2(void)
-//{
-//	j = 0;
-//	j++;
-//}
-//int main()
-//{
-//	for (int k = 0; k<10; k++)
+//	for (int i = 0; i < count - 1; i++)
 //	{
-//		fun1();
-//		fun2();
+//		for (int j = 0; j < count - 1 - i; j++)
+//		{
+//			if (cmp((char *)base + j * size, (char *)base + (j + 1) * size) > 0)
+//			{
+//				swap((char *)base + j * size, (char *)base + (j + 1) * size, size);
+//			}
+//		}
 //	}
-//	printf("%d\n", j);
-//	system("pause");
-//	return 0;
 //}
+//
 //int main()
 //{
-//	int n = 0;
-//	char *pc = (char *)&n;
-//	int *pi = &n;
-//	printf("%p\n", &n);
-//	printf("%p\n", pc);
-//	printf("%p\n", pc + 1);
-//	printf("%p\n", pi);
-//	printf("%p\n", pi + 1);
+//	int arr[] = { 10,9,8,7,6,5,4,3,2,1 };
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	bubble_sort(arr, sz, sizeof(arr[0]), int_cmp);
+//	for (int i = 0; i < sz; i++)
+//	{
+//		printf("%d ", arr[i]);
+//	}
 //	system("pause");
 //	return 0;
 //}
