5562b46ffb8234dbc2d138674ca78a788b70e761
The Laughing Uncle <33725245+1792114384@users.noreply.github.com>
2018-12-31 13:02:19 +0800

    Add files via upload

---
+++ 大小端判断/Project34/test.c
@@ -0,0 +1,19 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+#include <stdlib.h>
+
+//int main()
+//{
+//	int a = 1;
+//	if (*(char *)&a == 1)
+//	{
+//		printf("Ð¡¶Ë\n");
+//	}
+//	else
+//	{
+//		printf("´ó¶Ë\n");
+//	}
+//	system("pause");
+//	return 0;
+//}
